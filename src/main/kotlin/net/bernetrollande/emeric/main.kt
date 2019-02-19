0cc7fa3e89be09b0ac0150b4bbe1fa4c060e51f3
Soggard <e.bernet@laposte.net>
2019-02-19 11:55:39 +0100

    isole la connexion à la bdd

--- src/main/kotlin/net/bernetrollande/emeric/main.kt
+++ src/main/kotlin/net/bernetrollande/emeric/main.kt
@@ -2,6 +2,8 @@ package net.bernetrollande.emeric
 
 import io.ktor.application.call
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
@@ -13,53 +15,45 @@ import java.sql.DriverManager
 
 fun main() {
     embeddedServer(Netty, 8080) {
+        val connectionPool = ConnectionPool("jdbc:mysql://localhost/cms?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "")
+
         routing {
             get("/") {
-                //call.respondText("<h1>It's alive ! It's alive !", ContentType.Text.Html)
 
-                val connection = databaseConnection()
-                val stmt = connection!!.prepareStatement("SELECT * FROM article")
-                val results = stmt.executeQuery()
+                connectionPool.use { connection ->
+                    val stmt = connection.prepareStatement("SELECT * FROM article")
+                    val results = stmt.executeQuery()
 
-                val str = buildString {
-                    while (results.next()) {
-                        val id = results.getInt("id")
-                        val title = results.getString("title")
-                        append("<p><a href=\"/article/$id\">$title</a></p>")
+                    val str = buildString {
+                        while (results.next()) {
+                            val id = results.getInt("id")
+                            val title = results.getString("title")
+                            append("<p><a href=\"/article/$id\">$title</a></p>")
+                        }
                     }
+                    call.respondText(str, ContentType.Text.Html)
                 }
-                call.respondText(str, ContentType.Text.Html)
-
             }
 
             get("/article/{id}") {
-                //call.respondText("<h1>It's alive ! It's alive !", ContentType.Text.Html)
+                val id: String = call.parameters["id"] !!
 
-                val connection = databaseConnection()
-                val id = call.parameters["id"]
+                connectionPool.use { connection ->
+                    val s = connection.prepareStatement("SELECT title, text FROM article WHERE id = ?")
+                    s.setString(1, id)
 
-                val s = connection!!.prepareStatement("SELECT * FROM article WHERE id = ?")
-                s.setString(1, id)
+                    val results = s.executeQuery()
 
-                val results = s.executeQuery()
-
-                val str = buildString {
-                    while (results.next()) {
-                        val id = results.getInt("id")
+                    val found = results.next()
+                    if (found) {
                         val title = results.getString("title")
                         val text = results.getString("text")
-                        append("<h1>$title</h1><p>$text</p>")
+                        call.respondText("<h1>$title</h1><p>$text</p>", ContentType.Text.Html)
+                    } else {
+                        call.respond(HttpStatusCode.NotFound)
                     }
                 }
-
-                call.respondText(str, ContentType.Text.Html)
-
             }
         }
     }.start(true)
-}
-
-fun databaseConnection(): Connection? {
-    return DriverManager.getConnection("jdbc:mysql://localhost/cms?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "")
-
 }
